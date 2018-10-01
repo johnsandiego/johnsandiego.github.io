245947cb4eb779d99890817cc88375ff004775d7
JOHN SAN DIEGO <johnpatrick415@gmail.com>
2018-09-30 21:32:01 -0400

    added a prompt

--- controller/bodyCtrl.js
+++ controller/bodyCtrl.js
@@ -1,3 +1,23 @@
-angular.module("MyApp").controller('bodyCtrl', function($scope) {
+angular.module("MyApp").controller('bodyCtrl', function($scope, $mdDialog) {
+    $scope.name = "My First App";
+    $scope.inputValue = "";
 
+    $scope.openDialog = function(ev){
+        var confirm = $mdDialog.prompt()
+            .title('What would you name your dog?')
+            .textContent('Bowser is a common name.')
+            .placeholder('Dog name')
+            .ariaLabel('Dog name')
+            .initialValue('Buddy')
+            .targetEvent(ev)
+            .required(true)
+            .ok('Okay!')
+            .cancel('I\'m a cat person');
+
+        $mdDialog.show(confirm).then(function(result) {
+            $scope.status = 'You decided to name your dog ' + result + '.';
+        }, function() {
+            $scope.status = 'You didn\'t name your dog.';
+        });
+    };
 });

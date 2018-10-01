1c309bf123554b73862bea7f100b1cc19b62ad97
JOHN SAN DIEGO <johnpatrick415@gmail.com>
2018-09-30 21:05:55 -0400

    b

--- controller/app.js
+++ controller/app.js
@@ -1,9 +1,2 @@
 var app = angular.module("MyApp", ['ngMaterial', 'ngMessages']);
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
